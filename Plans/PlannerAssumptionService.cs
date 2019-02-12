a65836981e3091805ef2dcbcf66c912ca9bf8a48
shukladhaval25 <shukla_dhaval@yahoo.com>
2019-02-13 01:32:11 +0530

    add business logic for planner assumption.

--- Plans/PlannerAssumptionService.cs
+++ Plans/PlannerAssumptionService.cs
@@ -1,13 +1,9 @@
 ﻿using FinancialPlanner.Common;
 using FinancialPlanner.Common.Model;
 using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FinancialPlanner.BusinessLogic
 {
@@ -19,21 +15,23 @@ namespace FinancialPlanner.BusinessLogic
         const string SELECT_COUNT = "SELECT COUNT(*) FROM PLANNERASSUMPTION N1, USERS U WHERE N1.UPDATEDBY = U.ID AND N1.PID = {0}";
 
         const string INSERT_QUERY = "INSERT INTO PLANNERASSUMPTION VALUES (" +
-            "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},'{10}','{11}',{12},'{13}',{14})";
+            "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},'{10}','{11}',{12},'{13}',{14},'{15}',{16},{17},{18})";
 
         const string UPDATE_QUERY = "UPDATE PLANNERASSUMPTION SET " +
-            "ClientRetirementAge = {0}," + 
+            "ClientRetirementAge = {0}," +
             "SpouseRetirementAge = {1}," +
             "ClientLifeExpectancy = {2}," +
             "SpouseLifeExpectancy = {3}," +
             "PreRetirementInflactionRate ={4}," +
             "PostRetirementInflactionRate ={5}," +
             "EquityReturnRate = {6}," +
-            "DebtReturnRate = {7}," + 
+            "DebtReturnRate = {7}," +
             "OtherReturnRate = {8}," +
             "Description = '{9}'," +
             "UPDATEDON = '{10}'," +
-            "UPDATEDBY={11} WHERE ID ={12}";
+            "UPDATEDBY={11}, ConsiderClientAgeForRetirment = '{12}'," +
+            "ClientIncomeRise ={13}, SpouseIncomeRise = {14}," +
+            "OngoingExpRise ={15} WHERE ID ={16}";
 
         public PlannerAssumption GetAll(int plannerId)
         {
@@ -42,19 +40,19 @@ namespace FinancialPlanner.BusinessLogic
                 Logger.LogInfo("Get: PlannerAssumption process start");
                 PlannerAssumption plannerAssumption = new PlannerAssumption();
 
-                DataTable dtAppConfig =  DataBase.DBService.ExecuteCommand(string.Format(SELECT_ALL,plannerId));
+                DataTable dtAppConfig = DataBase.DBService.ExecuteCommand(string.Format(SELECT_ALL, plannerId));
                 foreach (DataRow dr in dtAppConfig.Rows)
                 {
-                   plannerAssumption = convertToPlannerAssumptionObject(dr);
+                    plannerAssumption = convertToPlannerAssumptionObject(dr);
                 }
                 Logger.LogInfo("Get: PlannerAssumption process completed.");
                 return plannerAssumption;
             }
             catch (Exception ex)
             {
-                StackTrace st = new StackTrace ();
-                StackFrame sf = st.GetFrame (0);
-                MethodBase  currentMethodName = sf.GetMethod();
+                StackTrace st = new StackTrace();
+                StackFrame sf = st.GetFrame(0);
+                MethodBase currentMethodName = sf.GetMethod();
                 LogDebug(currentMethodName.Name, ex);
                 return null;
             }
@@ -65,8 +63,8 @@ namespace FinancialPlanner.BusinessLogic
             try
             {
                 string clientName =
-                    DataBase.DBService.ExecuteCommandScalar(string.Format(GET_CLIENT_NAME_QUERY,PlannerAssumption.Pid));
-                string recordCount  = DataBase.DBService.ExecuteCommandScalar(string.Format(SELECT_COUNT,PlannerAssumption.Pid));
+                    DataBase.DBService.ExecuteCommandScalar(string.Format(GET_CLIENT_NAME_QUERY, PlannerAssumption.Pid));
+                string recordCount = DataBase.DBService.ExecuteCommandScalar(string.Format(SELECT_COUNT, PlannerAssumption.Pid));
                 DataBase.DBService.BeginTransaction();
                 if (recordCount != "0")
                 {
@@ -81,8 +79,12 @@ namespace FinancialPlanner.BusinessLogic
                        PlannerAssumption.DebtReturnRate,
                        PlannerAssumption.OtherReturnRate,
                        PlannerAssumption.Decription,
-                       PlannerAssumption.UpdatedOn.ToString("yyyy-MM-dd hh:mm:ss"), 
-                       PlannerAssumption.UpdatedBy, PlannerAssumption.Id), true);
+                       PlannerAssumption.UpdatedOn.ToString("yyyy-MM-dd hh:mm:ss"),
+                       PlannerAssumption.UpdatedBy,
+                       PlannerAssumption.IsClientRetirmentAgeIsPrimary,
+                       PlannerAssumption.ClientIncomeRise, PlannerAssumption.SpouseIncomeRise,
+                       PlannerAssumption.OngoingExpRise,
+                       PlannerAssumption.Id), true);
                 }
                 else
                 {
@@ -98,10 +100,14 @@ namespace FinancialPlanner.BusinessLogic
                       PlannerAssumption.DebtReturnRate,
                       PlannerAssumption.OtherReturnRate,
                       PlannerAssumption.Decription,
-                      PlannerAssumption.CreatedOn.ToString("yyyy-MM-dd hh:mm:ss"), 
+                      PlannerAssumption.CreatedOn.ToString("yyyy-MM-dd hh:mm:ss"),
                       PlannerAssumption.CreatedBy,
                       PlannerAssumption.UpdatedOn.ToString("yyyy-MM-dd hh:mm:ss"),
-                      PlannerAssumption.UpdatedBy, PlannerAssumption.Id), true);
+                      PlannerAssumption.UpdatedBy,
+                      PlannerAssumption.IsClientRetirmentAgeIsPrimary,
+                      PlannerAssumption.ClientIncomeRise, PlannerAssumption.SpouseIncomeRise,
+                      PlannerAssumption.OngoingExpRise,
+                      PlannerAssumption.Id), true);
                 }
                 Activity.ActivitiesService.Add(ActivityType.UpdatePlannerAssumption, EntryStatus.Success,
                             Source.Server, PlannerAssumption.UpdatedByUserName, clientName, PlannerAssumption.MachineName);
@@ -110,9 +116,9 @@ namespace FinancialPlanner.BusinessLogic
             catch (Exception ex)
             {
                 DataBase.DBService.RollbackTransaction();
-                StackTrace st = new StackTrace ();
-                StackFrame sf = st.GetFrame (0);
-                MethodBase  currentMethodName = sf.GetMethod();
+                StackTrace st = new StackTrace();
+                StackFrame sf = st.GetFrame(0);
+                MethodBase currentMethodName = sf.GetMethod();
                 LogDebug(currentMethodName.Name, ex);
                 throw ex;
             }
@@ -132,9 +138,13 @@ namespace FinancialPlanner.BusinessLogic
             plannerAssumption.EquityReturnRate = dr.Field<decimal>("EquityReturnRate");
             plannerAssumption.DebtReturnRate = dr.Field<decimal>("DebtReturnRate");
             plannerAssumption.OtherReturnRate = dr.Field<decimal>("OtherReturnRate");
-            plannerAssumption.Decription = dr.Field<string>("Description");          
+            plannerAssumption.Decription = dr.Field<string>("Description");
             plannerAssumption.UpdatedOn = dr.Field<DateTime>("UpdatedOn");
             plannerAssumption.UpdatedBy = dr.Field<int>("UpdatedBy");
+            plannerAssumption.IsClientRetirmentAgeIsPrimary = dr.Field<bool>("ConsiderClientAgeForRetirment");
+            plannerAssumption.ClientIncomeRise = dr.Field<decimal>("ClientIncomeRise");
+            plannerAssumption.SpouseIncomeRise = dr.Field<decimal>("SpouseIncomeRise");
+            plannerAssumption.OngoingExpRise = dr.Field<decimal>("OngoingExpRise");
             return plannerAssumption;
         }
 
