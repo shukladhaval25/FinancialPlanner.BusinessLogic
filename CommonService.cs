77c86342766e0bd91bf87ab9b2db20c3fb4fd6b2
shukladhaval25 <shukla_dhaval@yahoo.com>
2020-01-05 18:14:45 +0530

    Add logic for investment recommendation send.

---
+++ CommonService.cs
@@ -0,0 +1,25 @@
+﻿using FinancialPlanner.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialPlanner.BusinessLogic
+{
+    public class CommonService
+    {
+        public string GetString(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+                return string.Empty;
+            if (!System.IO.File.Exists(filePath))
+            {
+                Logger.LogDebug("File not found :" + filePath);
+                return string.Empty;
+            }
+
+            return Common.DataConversion.FileConversion.GetStringfromFile(filePath);
+        }
+    }
+}
--- PlanOption/InvestmentRecommendationService.cs
+++ PlanOption/InvestmentRecommendationService.cs
@@ -1,9 +1,13 @@
-﻿using FinancialPlanner.Common;
+﻿using FinancialPlanner.BusinessLogic.ApplictionConfiguration;
+using FinancialPlanner.Common;
 using FinancialPlanner.Common.Model;
 using FinancialPlanner.Common.Model.PlanOptions;
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 
 namespace FinancialPlanner.BusinessLogic.PlanOption
@@ -17,6 +21,12 @@ namespace FinancialPlanner.BusinessLogic.PlanOption
 
 
         const string DELETE_INVESTMENTRECOMMENDATIONRATIO = "DELETE FROM InvestmentRatio WHERE ID = {0}";
+
+        const string ADD_SEND_INVESTMENT_RECOMMENDATION = "INSERT" +
+            "[InvestmentRecommendationSend] ([PID],[SendDate],[ReportDataPath],[FileName]) VALUES ({0},'{1}','{2}','{3}')";
+
+        const string GET_SEND_INVESTMENT_RECOMMENDATION = "SELECT *   FROM[FinancialPlanner].[dbo].[InvestmentRecommendationSend] " +
+          "WHERE PID = {0}";
 
         public InvestmentRecommendationRatio Get(int pid)
         {
@@ -47,7 +57,7 @@ namespace FinancialPlanner.BusinessLogic.PlanOption
         {
             try
             {
-                //DataBase.DBService.BeginTransaction();
+                DataBase.DBService.BeginTransaction();
 
                 DataBase.DBService.ExecuteCommandString(string.Format(DELETE_INVESTMENTRECOMMENDATIONRATIO, investmentRecommendationRatio.Pid));
 
@@ -66,6 +76,57 @@ namespace FinancialPlanner.BusinessLogic.PlanOption
             catch (Exception ex)
             {
                 DataBase.DBService.RollbackTransaction();
+                StackTrace st = new StackTrace();
+                StackFrame sf = st.GetFrame(0);
+                MethodBase currentMethodName = sf.GetMethod();
+                LogDebug(currentMethodName.Name, ex);
+                throw ex;
+            }
+        }
+
+        public IList<InvRecommendationSend> GetSendReports(int plannerId)
+        {
+            IList<InvRecommendationSend> recommendationSends = new List<InvRecommendationSend>();
+
+            DataTable dtAppConfig = DataBase.DBService.ExecuteCommand(string.Format(GET_SEND_INVESTMENT_RECOMMENDATION,plannerId));
+            foreach (DataRow dr in dtAppConfig.Rows)
+            {
+                InvRecommendationSend recommendationSend = convertToInvRecSendObject(dr);
+                recommendationSends.Add(recommendationSend);
+            }
+            return recommendationSends;
+        }
+
+        private InvRecommendationSend convertToInvRecSendObject(DataRow dr)
+        {
+            InvRecommendationSend recommendationSend = new InvRecommendationSend();
+            recommendationSend.Id = dr.Field<int>("Id");
+            recommendationSend.Pid = dr.Field<int>("PID");
+            recommendationSend.SendDate = dr.Field<DateTime>("SendDate");
+            recommendationSend.ReportDataPath = dr.Field<string>("ReportDataPath");
+            recommendationSend.FileName = dr.Field<string>("FileName");
+            return recommendationSend;
+        }
+
+        public void AddSendReport(InvRecommendationSend invRecommendationSend)
+        {
+            try
+            {
+               // DataBase.DBService.BeginTransaction();
+                string filePath = getInvestmentRecommendationSendReportPath(invRecommendationSend.ClientId.ToString(), invRecommendationSend.Pid.ToString());
+
+                if (invRecommendationSend.ReportDataPath != null)
+                {
+                    byte[] arrBytes = Convert.FromBase64String(invRecommendationSend.ReportDataPath);
+                    File.WriteAllBytes(filePath, arrBytes);
+                }
+                
+                DataBase.DBService.ExecuteCommandString(string.Format(ADD_SEND_INVESTMENT_RECOMMENDATION, invRecommendationSend.Pid, invRecommendationSend.SendDate.ToString("yyyy-MM-dd hh:mm:ss"), filePath, Path.GetFileName(filePath)));
+               // DataBase.DBService.CommitTransaction();
+            }
+            catch(Exception ex)
+            {
+                //DataBase.DBService.RollbackTransaction();
                 StackTrace st = new StackTrace();
                 StackFrame sf = st.GetFrame(0);
                 MethodBase currentMethodName = sf.GetMethod();
@@ -125,6 +186,31 @@ namespace FinancialPlanner.BusinessLogic.PlanOption
             debuggerInfo.Method = methodName;
             debuggerInfo.ExceptionInfo = ex;
             Logger.LogDebug(debuggerInfo);
+        }
+
+        private string getInvestmentRecommendationSendReportPath(string clientId, string plannerId)
+        {
+            string applicationPath = getApplicationPath();
+
+            if (applicationPath == null)
+                return null;
+            if (!Directory.Exists(Path.Combine(applicationPath, clientId, plannerId)))
+            {
+                System.IO.Directory.CreateDirectory(
+                    Path.Combine(applicationPath, clientId, plannerId));
+            }
+            string fileName = "InvRec - " + DateTime.Now.Date.ToString("dd-MM-yyyy") + ".pdf";
+            return Path.Combine(applicationPath, clientId,plannerId,fileName );
+        }
+
+        private string getApplicationPath()
+        {
+            ApplicationConfiService appConfig = new ApplicationConfiService();
+            IList<ApplicationConfiguration> appConfigs = appConfig.Get();
+            var resultConfig = appConfigs.First(i => i.SettingName == "Application Path");
+            if (resultConfig != null)
+                return resultConfig.SettingValue.ToString();
+            return null;
         }
     }
 }
